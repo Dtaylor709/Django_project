5ca1657e5389693d8bcce6a5105253530ba68923
Dtaylor709 <84775234+Dtaylor709@users.noreply.github.com>
2021-07-27 20:58:25 +0800

    User app setup

--- she_codes_news/news/admin.py
+++ she_codes_news/news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import NewsStory
 # Register your models here.
+
+class NewsStoryAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'title', 'content', 'author', 'image_url']
+
+admin.site.register(NewsStory, NewsStoryAdmin)
